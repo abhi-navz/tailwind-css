2d54c5c6ad6160cd38cad523b9d8dbbf2de02723
abhi-navz <abhinavg01441@gmail.com>
2023-06-24 11:34:25 +0530

    Card Designing and responsiveness using tailwind

--- Project_1/tailwind.config.js
+++ Project_1/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/index.html"],
+  content: ["./dist/*.html"], // changed index.html to *.html so that it works on every .html file in directory
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
